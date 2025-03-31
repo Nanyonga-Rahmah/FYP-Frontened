e1013c03fc85a6ce33745289e2949b25d5414bfe
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2025-03-31 16:26:54 +0300

    w

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,2 +1,3 @@
 export const Register = `${import.meta.env.VITE_BACKEND_URL}/auth/register`;
 export const Login = `${import.meta.env.VITE_BACKEND_URL}/auth/signin`;
+export const FarmCreate = `${import.meta.env.VITE_BACKEND_URL}/farm/create`;
---
+++ src/pages/Map.tsx
@@ -0,0 +1,10 @@
+import FarmCreationMap from "@/components/Farmers/FarmCreationMap";
+function Map() {
+  return (
+    <div className="flex justify-center items-center min-h-screen ">
+      <FarmCreationMap />
+    </div>
+  );
+}
+
+export default Map;
