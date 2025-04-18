eb00f1001db472b219de720f5148862866d94fde
nserekolauv101 <nserekolauv101@gmail.com>
2025-04-18 16:43:42 +0000

    update harvest addition

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -3,6 +3,8 @@ export const Login = `${import.meta.env.VITE_BACKEND_URL}/auth/signin`;
 export const FarmCreate = `${import.meta.env.VITE_BACKEND_URL}/farm/create`;
 export const AllFarms = `${import.meta.env.VITE_BACKEND_URL}/farm/all`;
 export const viewOneFarm = `${import.meta.env.VITE_BACKEND_URL}/farm/`;
+export const HarvestCreate = `${import.meta.env.VITE_BACKEND_URL}/harvests/`;
+export const deleteFarm = `${import.meta.env.VITE_BACKEND_URL}/farm/delete/`;
 
 
 
